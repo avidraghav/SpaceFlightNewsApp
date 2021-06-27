e15316b51ca672b234942c04449f0e339a552d1c
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2021-06-27 21:53:22 +0530

    Added SearchArticle Fragment to navigation

--- app/src/main/java/com/example/spaceflightnewsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/spaceflightnewsapp/ui/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.spaceflightnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.spaceflightnewsapp.R
+import com.example.spaceflightnewsapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+           binding.bottomNavigationView.setupWithNavController(findNavController(R.id.navHostFragment))
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/spaceflightnewsapp/ui/SearchArticleFragment.kt
@@ -0,0 +1,11 @@
+package com.example.spaceflightnewsapp.ui
+
+
+import androidx.fragment.app.Fragment
+import com.example.spaceflightnewsapp.R
+
+
+class SearchArticleFragment : Fragment(R.layout.fragment_search_article) {
+
+
+}
